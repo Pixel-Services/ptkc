fddabd18952bcb5f83585ab2ee36b4c205af68e5
sieadev <office@siea.dev>
2025-03-24 10:30:59 +0100

    mhm

--- src/main/java/com/pixelservices/config/ConfigFactory.java
+++ src/main/java/com/pixelservices/config/ConfigFactory.java
@@ -1,10 +1,25 @@
 package com.pixelservices.config;
 
+/**
+ * The ConfigFactory class provides factory methods for creating configuration objects.
+ */
 public class ConfigFactory {
+
+    /**
+     * Returns a new YamlConfig instance of the default configuration file.
+     *
+     * @return a new YamlConfig instance.
+     */
     public static YamlConfig getYamlConfig() {
         return new YamlConfig();
     }
 
+    /**
+     * Returns a new YamlConfig instance with a specified file path.
+     *
+     * @param path the path to the configuration file.
+     * @return a new YamlConfig instance.
+     */
     public static YamlConfig getYamlConfig(String path) {
         return new YamlConfig(path);
     }
